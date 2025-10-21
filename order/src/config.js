08a32ebcdeadfd3b99c0105c606aabf0d8fc650c
Tran My Hanh <tmyhanh870@gmail.com>
2025-10-21 22:55:25 +0700

    update_prj

--- order/src/config.js
+++ order/src/config.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 module.exports = {
     mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
-    rabbitMQURI: 'amqp://localhost',
+    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://rabbitmq:5672',
+    // rabbitMQURI: 'amqp://localhost',
     rabbitMQQueue: 'orders',
     port: 3002
 };
